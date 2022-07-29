68c5531ba0add3bc45df0b70fc58a074a5d2fc9a
Lloyd W Turk Jr <xerodill@gmail.com>
2022-07-29 09:16:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# MegaMillions quickpick and frequency analyzer
+This script aims to analyze the frequency of MegaMillions numbers appearing and generate quick picks using pseudo-random number generation
+## Getting Started
+Download script, install requirements using pip, I think the only one that isn't standard instal with python is pandas, however they are all listed in the requirements.txt
+### Prerequisites
+python 3 (tested with Python 3.10.5)
+pandas python lib (pip install pandas)
+### Additional info
+This is a modified version of my Powerball script in honor of MegaMillions getting to 1.1B jackpot. Eventually I would like to add "Hot" and "Cold" picks. Basically numbers that seem to appear more frequently than others, and ones that don't seem to appear as often, also ones that are seemingly "overdue" to be picked given the number picking is completely random all numbers should in theory have the same odds of being picked. (1 in 70 for the white balls, and 1 in 25 for the Megaball) in any given order. If I ever get to implementing that I will add it to the Powerball script as well. Feel free to add it yourself as well of course.
+
---
+++ mega_millions.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python
+"""
+#==============================================================================================
+# megamillions Frequency analyzer and quick pick
+# basically a python re-write of
+# Mega Millions version of https://github.com/lwturkjr/Python_Powerball
+# There isn't much difference aside the number ranges used, and the latest rule change
+# was in October of 2017
+# Author: Lloyd Turk Jr.
+#==============================================================================================
+"""
+
+import random
+import os
+#from base64 import b64encode
+#import urllib
+import urllib.request as ur
+import json
+#import time
+from datetime import datetime, date
+#from collections import defaultdict
+
+import pandas as pd
+
+#START_TIME = time.time()
+"""
+#SYSTEM_RANDOM = os.urandom # Gives DeprecationWarning: Seeding based on hashing is deprecated
+since Python 3.9 and will be removed in a subsequent version. The only 
+supported seed types are: None, int, float, str, bytes, and bytearray.
+random.seed(SYSTEM_RANDOM)
+"""
+SYSTEM_RANDOM = os.urandom(64) # Seems to resolve this
+#seed = b64encode(SYSTEM_RANDOM).decode('utf-8') # This was another option to resolve it then random.seed(str(seed))
+random.seed(SYSTEM_RANDOM)
+
+def get_drawing_history():
+    """Get the data from online"""
+    # https://catalog.data.gov/dataset/lottery-mega-millions-winning-numbers-beginning-2002
+    url = "https://data.ny.gov/api/views/5xaw-6ayf/rows.json" # It's on data.gov from NY
+    response = ur.urlopen(url)
+    json_data = json.loads(response.read())
+    #print(json_data.keys())
+
+    raw_data = json_data["data"]
+    raw_list = [] #
+    for i in raw_data:
+        raw_list.extend((i[8], i[9]))
+
+    chunked_data_list = []
+    for i in range(0, len(raw_list), 2):
+        chunk = raw_list[i:i + 2]
+        chunked_data_list.append(chunk)
+
+    final_data_list = []
+    for i in chunked_data_list: # This works, it seems like it could still be more optimized though
+        raw_date_time_str = i[0] # Raw date/time string from the json
+        # Cleaned up date/time string
+        date_time_str = datetime.strptime(raw_date_time_str, "%Y-%m-%dT%H:%M:%S")
+        # Get rid of time part, we don't need it, I'm sure there is an easier/lighter way to do this
+        # but this works for now
+        date_str = date_time_str.strftime("%Y-%m-%d")
+
+        final_data_list.append([date_str, i[1]])
+
+    #print(final_data_list)
+    return final_data_list
+
+def quick_pick():
+    """Quickpick function"""
+    nums = []
+    count = 1
+    while count <= 5: # we need 5 numbers
+        pick = random.randint(1, 70) # From 1 to 70
+        # If pick isn't in the nums list, append the pick to the nums list,
+        # it would return occasional dupes, this should fix that.
+        if pick not in nums:
+            nums.append(pick) # Append them to the list
+            count = count+1
+        else: # Don't add anything to count
+            pass
+    nums.sort() # Sort list for readability, makes it easier when filling out the number slips
+
+    mega_ball = random.randint(1, 25)
+
+    print("========================================================================")
+    print("Random quick pick numbers: " + str(nums) +" "+ str(mega_ball))
+    #print("========================================================================")
+
+def get_dates():
+    """Analyze frequency of numbers"""
+    # For custom date input
+    #oldest_date = str(input("Input the oldest date in YYYY-MM-DD format: "))
+    #newest_date = str(input("Input the newest date in YYYY-MM-DD format:"))
+    #oldest_date = "2022-01-04" # First drawing of this year
+    oldest_date = "2017-10-28" # This is the latest rule change
+    #oldest_date = "2002-05-17" # The oldest date the data set goes back to
+    #newest_date = "2022-07-26" # If you want to define a specific date range
+
+    today = date.today()
+
+    dates = pd.date_range(start=oldest_date, end=today) # Get a date range using pandas
+    #dates = pd.date_range(start=oldest_date, end=newest_date) # For custom date range
+
+    return dates
+
+def dates_to_list():
+    """Converts the pandas date_range into a list. This might not be necessary?"""
+    dates = get_dates()
+    date_list = []
+    for date_time_obj in dates:
+        date_str = date_time_obj.strftime("%Y-%m-%d")
+        date_list.append(date_str)
+
+    return date_list
+
+def get_split_ball_list():
+    """Split up the ball list to work with in later functions"""
+    data = get_drawing_history()
+    date_list = dates_to_list()
+
+    ball_list = [] # This is a list, of strings being "int int int int int int"
+    for i in data:
+        if i[0] in date_list:
+            ball_list.append(i[1])
+
+    # We split these strings into lists of strings ["int", "int", "int", "int", "int", "int"], [...]
+    split_list = []
+    for i in ball_list:
+        split = i.split()
+        split_list.append(split)
+
+    split_ball_list = [] # We turn this list of lists into a single list of strings
+    for i in split_list:
+        for j in i:
+            split_ball_list.append(int(j))
+            # We want the data in the list to be int, to use comparative operations later
+
+    return split_ball_list
+
+def get_ball_frequency():
+    """"Analyze the frequency of balls being picked"""
+    split_ball_list = get_split_ball_list()
+
+    mb_list = split_ball_list[5::6] # Megaball is going to be every 6th entry in the list
+
+    # To get white ball nums we now delete every 6th entry, which is the megamillions
+    del split_ball_list[5::6]
+
+    white_ball_list = split_ball_list # Assign to a new list name, for ease of programming
+
+
+    white_ball_list.sort() # Sort the list, this makes the dict we convert it to later more readable
+    mb_list.sort() # Sort the list, this makes the dict we convert it to later more readable
+
+    #unique_mb_list = list(set(mb_list))
+    #unique_wb_list = list(set(white_ball_list))
+
+    white_ball_dict = {i:white_ball_list.count(i) for i in white_ball_list}
+    mb_dict = {i:mb_list.count(i) for i in mb_list}
+
+    # Get MegaBall values and number of times they've been drawn
+    mb_max_value = max(mb_dict.values()) # Find the most commonly drawn MegaBall number
+
+    # Maximum secondary value for MB
+    mb_max_secondary = 0
+    for i in mb_dict.values(): # Find the second most commonly drawn MegaBall number
+        if(i > mb_max_secondary and i < mb_max_value):
+            mb_max_secondary = i
+
+    print("========================================================================")
+    print("All numbers drawn for MegaBall with number of times drawn, in given date range")
+    print("Number:Number of times drawn")
+    print(mb_dict)
+    #for key, value in mb_dict.items(): # Print all MB and number of times drawn
+    #    print(str(key) + ":" + str(value))
+    print("========================================================================")
+    print("Most commonly drawn MegaBalls(s) since given date: ")
+    print("Number:Number of times drawn")
+    for key, value in mb_dict.items():
+        if value == mb_max_value: # Print MB(s) with highest draw rate
+            print(str(key) + ":" + str(value))
+    print("========================================================================")
+    print("Second most commonly drawn MegaBalls(s) since given date: ")
+    print("Number:Number of times drawn")
+    for key, value in mb_dict.items():
+        if value == mb_max_secondary: # Print MB(s) with second highest draw rate
+            print(str(key) + ":" + str(value))
+    print("========================================================================")
+
+    # Get white ball values and number of times they've been drawn
+    wb_max_value = max(white_ball_dict.values())
+    # Maximum secondary value for MB
+    wb_max_secondary = 0
+    for i in white_ball_dict.values(): # Find the second most commonly drawn white ball numbers
+        if(i > wb_max_secondary and i < wb_max_value):
+            wb_max_secondary = i
+    print("All numbers drawn with number of times drawn, in given date range")
+    print("Number:Number of times drawn")
+    print(white_ball_dict)
+    #for key, value in white_ball_dict.items(): # Print all white balls and number of times drawn
+    #    print(str(key) + ":" + str(value))
+    print("========================================================================")
+    print("Most commonly drawn number(s) since given date: ")
+    print("Number:Number of times drawn")
+    for key, value in white_ball_dict.items():
+        if value == wb_max_value: # Print wb(s) with highest draw rate
+            print(str(key) + ":" + str(value))
+    print("========================================================================")
+    print("Second most commonly drawn number(s) since given date: ")
+    print("Number:Number of times drawn")
+    for key, value in white_ball_dict.items():
+        if value == wb_max_secondary: # Print wb(s) with second highest draw rate
+            print(str(key) + ":" + str(value))
+    print("========================================================================")
+    print('This program is just for frequency analysis and a "Quick Pick" random'
+          '\nnumber generator making no pretense at predictive accuracy')
+
+
+quick_pick()
+get_ball_frequency()
+
+# For debugging to make sure it didn't take too long to run
+#print("--- %s seconds ---" % (time.time() - START_TIME))
